5a13696a6c8f07167bb2019cd30954eddd2dd9ed
matthewvolk <volkmattj@gmail.com>
2018-01-14 13:28:56 -0800

    added functions within user model to get user by id and username

--- models/user.js
+++ models/user.js
@@ -23,3 +23,13 @@ const UserSchema = mongoose.Schema({
 
 // export schema
 const User = module.exports = mongoose.model('User', UserSchema);
+
+// functions for getting users by ID and username
+module.exports.getUserById = function(id, callback) {
+    User.findById(id, callback);
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+    const Query = {username: username}
+    User.findOne(query, callback);
+};
