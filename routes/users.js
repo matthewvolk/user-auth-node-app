86452691d16314549862a99013cf8df6ff80892a
matthewvolk <volkmattj@gmail.com>
2018-01-14 13:46:24 -0800

    added user registration route handling

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
+// import modules
 const express = require('express');
 const router = express.Router();
+const passport = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // registeration route
 router.post('/register', (req, res, next) => {
-    res.send('REGISTRATION')
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if (err) {
+            res.json({success: false, msg: 'Failed to register user.'});
+        } else {
+            res.json({success: true, msg: 'User registered successfully.'});
+        }
+    });
 });
 
 // authentication route
