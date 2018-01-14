f3944792ec25e3a65136ccaf3876715dbe02a663
matthewvolk <volkmattj@gmail.com>
2018-01-14 13:25:47 -0800

    added user schema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('../models/database');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// export schema
+const User = module.exports = mongoose.model('User', UserSchema);
