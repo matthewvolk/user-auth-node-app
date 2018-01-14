5a323bd71b32d894f885164f25816603f20f92fb
matthewvolk <volkmattj@gmail.com>
2018-01-14 13:22:35 -0800

    set up user model directory

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // registeration route
-router.get('/register', (req, res, next) => {
+router.post('/register', (req, res, next) => {
     res.send('REGISTRATION')
 });
 
@@ -14,11 +14,6 @@ router.post('/authenticate', (req, res, next) => {
 // user profile route
 router.get('/profile', (req, res, next) => {
     res.send('PROFILE')
-});
-
-// validate route
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE')
 });
 
 // export module
