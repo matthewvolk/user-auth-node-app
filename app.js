dd73a6f4d9e21c7bbc4d202e7c708aecc22e505b
matthewvolk <volkmattj@gmail.com>
2018-01-14 18:32:15 -0800

    Fixes #1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express     = require('express');
 const path        = require('path');
 const bodyParser  = require('body-parser');
 const cors        = require('cors');
-const passport    = require('passport-jwt');
+const passport    = require('passport');
 const mongoose    = require('mongoose');
 const config      = require('./config/database');
 
